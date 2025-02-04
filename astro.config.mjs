7cf56637c75de3a7b06c5b82894ea0441e663613
hy7721 <azipark7721@gmail.com>
2025-02-04 17:38:17 +0900

    Update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig(
-  {
-    integrations: [tailwind()],
-  }, // your configuration options here...
-  // https://docs.astro.build/en/reference/configuration-reference/
+export default defineConfig({
+    site: 'https://hykelly7721.vercel.app',
+    output: "static",
+    integrations: [
+      tailwind()
+    ],
+  },
 );
